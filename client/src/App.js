64ff4fa53224b7aeb5d8279216722ba0f3c13e16
Michael Chop <007micle007@gmail.com>
2023-02-23 23:20:59 +0300

    Added context to the project

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,22 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import UpdatePage from "./pages/UpdatePage";
 import DetailPage from "./pages/DetailPage";
+import {RestaurantsContextProvider} from "./context/RestaurantsContext";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div className={"container"}>
-                <Routes>
-                    <Route exact path="/" element={<Home/>}/>
-                    <Route exact path="/restaurants/:id" element={<DetailPage/>}/>
-                    <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
-                </Routes>
-            </div>
+            <RestaurantsContextProvider>
+
+                <div className={"container"}>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>}/>
+                        <Route exact path="/restaurants/:id" element={<DetailPage/>}/>
+                        <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
+                    </Routes>
+                </div>
+                
+            </RestaurantsContextProvider>
         </BrowserRouter>
     );
 }
